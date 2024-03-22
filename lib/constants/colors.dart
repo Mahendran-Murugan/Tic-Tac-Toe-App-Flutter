84603e33d303fda1e5f4f4b7bf287ca6babaf15a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-22 19:21:10 +0530

    chore: remove comments

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static Color? primaryColor = Colors.grey[50];
+  static Color secondaryColor = Colors.grey;
+  static Color? accentColor = Colors.red[400];
+}
